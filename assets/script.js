a692db126b049ce50b46c20ce49c506f11708a5b
Jake <jacobartz1987@gmail.com>
2019-04-24 20:01:15 -0600

    Update, simplified for kids.

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,36 @@
-var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var guessed=[]
-var wins=0
-var losses=0
-var guesses=9
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var wins = 0
+var losses = 0
+var guesses = 9
 
 console.log("sdfasdf");
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
 
-    var userGuess = event.key; 
+    var playerLetter = event.key;
 
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    var computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-if (userGuess === computerGuess) {
-    wins++; guesses = 9; guessed = []
-}
-else if (userGuess != computerGuess){
-     guesses--; guessed.push(userGuess); 
-}
-if (guesses === 0) {
-    losses++; guesses = 9; guessed = []
-}
-document.getElementById("pGuess").innerHTML = " "+userGuess;
-document.getElementById("winCount").innerHTML = " "+wins;
-document.getElementById("lossCount").innerHTML = " "+losses;
-document.getElementById("remaining").innerHTML = " "+guesses;
-document.getElementById("soFar").innerHTML = " "+guessed;
+    if (computerChoices.includes(playerLetter)) {
+
+        if (playerLetter === computerLetter) {
+            wins++; guesses = 9; 
+            alert('You got it!');
+        }
+        else if (playerLetter != computerLetter) {
+            guesses--; 
+        }
+        if (guesses === 0) {
+            losses++; guesses = 9; guessed = []
+        }
+    } else {
+        alert('You must choose a letter!');
+    }
+
+    document.getElementById("player-guess").innerHTML = "   " + playerLetter;
+    document.getElementById("win-counter").innerHTML = " " + wins;
+    document.getElementById("loss-counter").innerHTML = " " + losses;
+    document.getElementById("remaining").innerHTML = " " + guesses;
+    
 }
 
